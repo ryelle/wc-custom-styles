ca5bc9df86b7bb6a5e8510030f17a3d042d2fc34
Kelly Dwan <ryelle@users.noreply.github.com>
2019-05-05 13:48:27 -0400

    Persist colors

--- src/store/effects.js
+++ src/store/effects.js
@@ -10,6 +10,27 @@ import { __, sprintf } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 import { dispatch } from '@wordpress/data';
 
+function saveColors( action, { getState } ) {
+	const { colors } = getState();
+	apiFetch( {
+		path: '/wc-cbs/v1/colors',
+		data: colors,
+		method: 'POST',
+	} )
+		/* We don't need to do anything… */
+		.then()
+		.catch( ( error ) => {
+			/* We need to handle the error. */
+			const message = sprintf(
+				__( 'Error saving colors: %s', 'wc-custom-block-styles' ),
+				error.message
+			);
+			dispatch( 'core/notices' ).createNotice( 'warning', message, {
+				isDismissible: true,
+			} );
+		} );
+}
+
 function saveStyles( action, { getState } ) {
 	const { styles } = getState();
 	apiFetch( {
@@ -32,6 +53,9 @@ function saveStyles( action, { getState } ) {
 }
 
 export default {
+	ADD_COLOR: debounce( saveColors, 250 ),
+	DELETE_COLOR: debounce( saveColors, 250 ),
+	UPDATE_COLOR: debounce( saveColors, 250 ),
 	ADD_STYLE: debounce( saveStyles, 250 ),
 	DELETE_STYLE: debounce( saveStyles, 250 ),
 	UPDATE_STYLE: debounce( saveStyles, 250 ),
